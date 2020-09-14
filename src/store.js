a3d550a8cd079e268ae858990a3cf53fcc0501d2
dnjsvltm201311681 <dnjsvltm201311681@gmail.com>
2020-09-14 22:30:06 +0900

    reduxjs/toolkit install & createAction

--- src/store.js
+++ src/store.js
@@ -1,21 +1,8 @@
 import { createStore } from "redux";
+import { createAction } from "@reduxjs/toolkit";
 
-const ADD = "ADD";
-const DELETE = "DELETE";
-
-const addToDo = (text) => {
-  return {
-    type: ADD,
-    text,
-  };
-};
-
-const deleteToDo = (id) => {
-  return {
-    type: DELETE,
-    id,
-  };
-};
+const addToDo = createAction("ADD");
+const deleteToDo = createAction("DELETE");
 
 export const actionCreators = {
   addToDo,
@@ -24,10 +11,11 @@ export const actionCreators = {
 
 const reducer = (state = [], action) => {
   switch (action.type) {
-    case ADD:
-      return [{ text: action.text, id: Date.now() }, ...state];
-    case DELETE:
-      return state.filter((toDo) => toDo.id !== action.id);
+    case addToDo.type:
+      console.log(action);
+      return [{ text: action.payload, id: Date.now() }, ...state];
+    case deleteToDo.type:
+      return state.filter((toDo) => toDo.id !== action.payload);
     default:
       return state;
   }
